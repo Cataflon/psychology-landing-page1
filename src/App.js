54a89f8d0f2f8348f143c69c9e0e5dbb2ae2868f
Cataflon <111638171+Cataflon@users.noreply.github.com>
2026-03-22 16:09:14 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 import AboutMe from "./pages/AboutMe/AboutMe";
 import Services from "./pages/Services/Services";
