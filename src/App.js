b35c51fd6bc00ae1609b1ae93a10cd4968e7a657
Cataflon <111638171+Cataflon@users.noreply.github.com>
2026-04-19 18:56:32 +0300

    figuring out hosting problems

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import AboutMe from "./pages/AboutMe/AboutMe";
 import Services from "./pages/Services/Services";
 import Consiliere from "./pages/Services/Consiliere/Consiliere";
-import  HomePage  from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 const App = () => {
   // home page
   // about me
@@ -11,16 +11,18 @@ const App = () => {
   //contact
   // reservation
   return (
+    <BrowserRouter basename="/psychology-landing-page1">
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/about" element={<AboutMe/>} />
-        <Route path="/consiliere" element={<Consiliere/>} />
-         <Route path="/consiliere/services" element={<Services />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/consiliere" element={<Consiliere />} />
+        <Route path="/consiliere/services" element={<Services />} />
       </Routes>
+    </BrowserRouter>
     // <div className="content">
     //   <AboutMe/>
     // </div>
   );
-}
+};
 
 export default App;
